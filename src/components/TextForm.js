0a0ea3f19a44c79396ddfb9fa018af814c59a779
kunal <kunal20vijay00@gmail.com>
2021-12-10 16:21:15 +0530

    Adding text summary and preview to TextUtils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,19 +4,35 @@ export default function TextForm(props) {
     const handleOnChange = (event) => {
         setText(event.target.value);
     }
-    const handleOnClick = () => {
+    const handleUpClick = () => {
         let newTxt=text.toUpperCase();
         setText(newTxt);
     }
+    const handleLoClick = () => {
+        let newTxt=text.toLowerCase();
+        setText(newTxt);
+    }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState('');
     return (
-        <div>
+        <>
+        <div className="container my-3">
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange} rows="10"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} placeholder='Enter yout text here ...' rows="10"></textarea>
             </div>
-            <button type="button" className="btn btn-primary" onClick={handleOnClick}>Click to Uppercase</button>
+            <button type="button" className="btn btn-primary mx-1" onClick={handleUpClick}>Click to Uppercase</button>
+            <button type="button" className="btn btn-primary mx-1" onClick={handleLoClick}>Click to Lowercase</button>
         </div>
+        <div className="container">
+            <h2>Text Summary</h2>
+            <h6>{text.split(' ').length} words and {text.length} characters</h6>
+            <h6>{0.008* text.split(' ').length} minutes needed to read</h6>
+        </div>
+        <div className="container">
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
