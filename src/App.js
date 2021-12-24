c839a6b2107aa164c99c83ca5e4ef0643a333d22
kunal <kunal20vijay00@gmail.com>
2021-12-24 19:14:37 +0530

    commented unnessary code and added build folder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 import Alert from './components/Alert';
 import React, { useState } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch
+// } from "react-router-dom";
 
 
 function App() {
@@ -42,22 +42,22 @@ function App() {
   }
 
   return (
-    <Router>
+    // <Router>
       <>
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container">
-          <Switch>
+          {/* <Switch>
             <Route exact path="/about">
               <About />
             </Route>
-            <Route exact path="/">
+            <Route exact path="/"> */}
               <TextForm showAlert={showAlert} heading='Enter Your Text' mode={mode} />
-            </Route>
-          </Switch>
+            {/* </Route>
+          </Switch> */}
         </div>
       </>
-    </Router>
+    // </Router>
   );
 }
 
